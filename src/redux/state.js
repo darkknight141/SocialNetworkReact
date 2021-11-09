0884bdc9a71bd174ac116ec2218a9658d2d6537c
darkknight141 <sashaannenkov4@gmail.com>
2021-11-09 21:14:55 +0300

    add "store"

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,51 +1,56 @@
-let rerender = () => {
-  console.log("change");
-};
-
-let state = {
-  posts: [
-    {
-      message: 'Hello everybody in social network "Redux"',
-      user: "Unknown User",
-      nickname: "@unknownuser99",
-      scr: "https://yt3.ggpht.com/a/AATXAJwiglRIge7WBIk1SHb7xAO8GroQnb2i4XlJ9b-P=s900-c-k-c0x00ffffff-no-rj",
+let store ={
+    _state: {
+        posts: [
+          {
+            message: 'Hello everybody in social network "Redux"',
+            user: "Unknown User",
+            nickname: "@unknownuser99",
+            scr: "https://yt3.ggpht.com/a/AATXAJwiglRIge7WBIk1SHb7xAO8GroQnb2i4XlJ9b-P=s900-c-k-c0x00ffffff-no-rj",
+          },
+          {
+            message: "I like learn React",
+            user: "Unknown User",
+            nickname: "@unknownuser99",
+            scr: "https://yt3.ggpht.com/a/AATXAJwiglRIge7WBIk1SHb7xAO8GroQnb2i4XlJ9b-P=s900-c-k-c0x00ffffff-no-rj",
+          },
+          {
+            message: "hehe",
+            user: "Anonymus",
+            nickname: "@anonymus",
+            scr: "https://www.wallpaperup.com/uploads/wallpapers/2016/02/22/897962/b1bd5007f0e11c83e1d4e57e4418d747-1400.jpg",
+          },
+        ],
+        newTextPost: "",
     },
-    {
-      message: "I like learn React",
-      user: "Unknown User",
-      nickname: "@unknownuser99",
-      scr: "https://yt3.ggpht.com/a/AATXAJwiglRIge7WBIk1SHb7xAO8GroQnb2i4XlJ9b-P=s900-c-k-c0x00ffffff-no-rj",
+    getState() {
+        return this._state
     },
-    {
-      message: "hehe",
-      user: "Anonymus",
-      nickname: "@anonymus",
-      scr: "https://www.wallpaperup.com/uploads/wallpapers/2016/02/22/897962/b1bd5007f0e11c83e1d4e57e4418d747-1400.jpg",
+    _rerender () {
+        console.log("change");
     },
-  ],
-  newTextPost: "",
-};
+    addPost () {
+        let newPost = {
+          message: this._state.newTextPost,
+          user: "Unknown User",
+          nickname: "@unknownuser99",
+          scr: "https://yt3.ggpht.com/a/AATXAJwiglRIge7WBIk1SHb7xAO8GroQnb2i4XlJ9b-P=s900-c-k-c0x00ffffff-no-rj",
+        };
+      
+        this._state.posts.push(newPost);
+        this._state.newTextPost='';
+        this._rerender(this._state);
+    },
+    updateNewPost (newPost) {
+        this._state.newTextPost = newPost;
+        this._rerender(this._state);
+    },
+    subscribe (observer) {
+        this._rerender = observer;
+    },
+}
 
-window.state = state;
-export const addPost = () => {
-  let newPost = {
-    message: state.newTextPost,
-    user: "Unknown User",
-    nickname: "@unknownuser99",
-    scr: "https://yt3.ggpht.com/a/AATXAJwiglRIge7WBIk1SHb7xAO8GroQnb2i4XlJ9b-P=s900-c-k-c0x00ffffff-no-rj",
-  };
 
-  state.posts.push(newPost);
-  rerender(state);
-};
+window.store = store;
 
-export const updateNewPost = (newPost) => {
-  state.newTextPost = newPost;
-  rerender(state);
-};
 
-export const subscribe = (observer) => {
-  rerender = observer;
-};
-
-export default state;
+export default store;
