e9fb8d861a79edce9512a90dbf2edec5a523fff5
darkknight141 <sashaannenkov4@gmail.com>
2021-11-13 20:44:11 +0300

    install "react-redux"

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import store from "./redux/reduxStore";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App"
 
 let rerender = (state) => {
   ReactDOM.render(
-    <App state={state} dispatch ={store.dispatch.bind(store)} store={store}/>,
-    document.getElementById("root")
+      <App state={state} dispatch={store.dispatch.bind(store)} store={store} />,
+      document.getElementById("root")
   );
 };
 
 rerender(store.getState());
-store.subscribe(()=>{
-  let state = store.getState()
+store.subscribe(() => {
+  let state = store.getState();
   rerender(state);
 });
