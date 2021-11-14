ab2d9d731ef7c13d81ac60944217b2bf36d4ddf4
darkknight141 <sashaannenkov4@gmail.com>
2021-11-14 18:00:19 +0300

    Add follow people

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -3,7 +3,35 @@ import s from './search.module.css';
 function Seacrh (){
     return(
     <div className={s.seacrh}>
-    
+        <div className='searchitem'>
+            <input placeholder='Find users' type="text" className={s.inputsearch}/>
+        </div>
+        <div className={s.users}>
+            <div className={s.useritem}>
+                <img className={s.photo} src="https://images.hdqwalls.com/download/cat-green-eyes-4k-i8-1024x768.jpg" />
+                <div className={s.info}>
+                    <p className='flname'>Jonny Catsville</p>
+                    <p className='nickname'>@jonnycat</p>
+                </div>
+                <button className={s.follow}>Follow</button>
+            </div>
+            <div className={s.useritem}>
+                <img className={s.photo} src="https://images.hdqwalls.com/download/cat-green-eyes-4k-i8-1024x768.jpg" />
+                <div className={s.info}>
+                    <p className='flname'>Jonny Catsville</p>
+                    <p className='nickname'>@jonnycat</p>
+                </div>
+                <button className={s.follow}>Follow</button>
+            </div>
+            <div className={s.useritem}>
+                <img className={s.photo} src="https://images.hdqwalls.com/download/cat-green-eyes-4k-i8-1024x768.jpg" />
+                <div className={s.info}>
+                    <p className='flname'>Jonny Catsville</p>
+                    <p className='nickname'>@jonnycat</p>
+                </div>
+                <button className={s.follow}>Follow</button>
+            </div>
+        </div>    
     </div>
 );
 }
