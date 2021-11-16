b8a86e9185141087c4288e9d28599c5f3a81f4d8
darkknight141 <sashaannenkov4@gmail.com>
2021-11-16 18:33:12 +0300

    1. Add "axios" for server request 2.Add render "users" from searchReducer

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,38 +1,44 @@
-import s from './search.module.css';
+import React from 'react';
+import style from './search.module.css';
+import User from './user';
+import * as axios from 'axios';
 
-function Seacrh (){
+function Search (props){
+    
+    
+    
+    // if(props.users.length === 0){
+    // axios.get('https:blablabla.com/api/1.0/users')       Работа с запросами на сервер(нужен бэк с какой то API)
+    // .then(response => {
+    //     props.setUsers(response.data.items)          
+    // });
+    // }
+
+
+
+
+    // console.log(props)
+    // let userElements = props.users.map( u => <User key={u.id} img={u.img} fullname={u.fullname} nickname={u.nickname} follower={u.follower} />)
+    
     return(
-    <div className={s.seacrh}>
+    <div className={style.seacrh}>
         <div className='searchitem'>
-            <input placeholder='Find users' type="text" className={s.inputsearch}/>
+            <input placeholder='Find users' type="text" className={style.inputsearch}/>
         </div>
-        <div className={s.users}>
-            <div className={s.useritem}>
-                <img className={s.photo} src="https://images.hdqwalls.com/download/cat-green-eyes-4k-i8-1024x768.jpg" />
-                <div className={s.info}>
-                    <p className='flname'>Jonny Catsville</p>
-                    <p className='nickname'>@jonnycat</p>
+        <div className={style.users}>
+        <div className={style.useritem}>
+            {props.users.map(u => <div key={u.id}>
+                <img className={style.photo} src={u.img} />
+                <div className={style.info}>
+                    <p className={style.flname}>{u.fullname}</p>
+                    <p className={style.nickname}>{u.nickname}</p>
                 </div>
-                <button className={s.follow}>Follow</button>
-            </div>
-            <div className={s.useritem}>
-                <img className={s.photo} src="https://images.hdqwalls.com/download/cat-green-eyes-4k-i8-1024x768.jpg" />
-                <div className={s.info}>
-                    <p className='flname'>Jonny Catsville</p>
-                    <p className='nickname'>@jonnycat</p>
-                </div>
-                <button className={s.follow}>Follow</button>
-            </div>
-            <div className={s.useritem}>
-                <img className={s.photo} src="https://images.hdqwalls.com/download/cat-green-eyes-4k-i8-1024x768.jpg" />
-                <div className={s.info}>
-                    <p className='flname'>Jonny Catsville</p>
-                    <p className='nickname'>@jonnycat</p>
-                </div>
-                <button className={s.follow}>Follow</button>
-            </div>
+                {u.follower ? <button onClick={()=>{props.unfollow(u.id)}} className={style.follow}>Unfollow</button> : <button onClick={()=>{props.follow(u.id)}} className={style.follow}>Follow</button>}
+            </div>)
+            }               
+        </div> 
         </div>    
     </div>
 );
 }
-export default Seacrh;
+export default Search;
