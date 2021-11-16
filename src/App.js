3781ed01659bb643efb87276fb1da8c5bc4c02df
darkknight141 <sashaannenkov4@gmail.com>
2021-11-16 18:30:20 +0300

    Refactoring code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Content from './components/content/content';
-import Nav from './components/navigation/nav';
-import Seacrh from './components/search/search';
-import Titlename from './components/title/title';
-import Music from './components/music/music';
-import News from './components/news/news';
-import Settings from './components/settings/settings'
-import {BrowserRouter, Routes,Route} from 'react-router-dom';
 import DialogsContainer from './components/messages/dialogsContainer';
+import Music from './components/music/music';
+import Nav from './components/navigation/nav';
+import News from './components/news/news';
+import Settings from './components/settings/settings';
+import Titlename from './components/title/title';
 import TitleNameSearch from './components/titleSearchName/titleSearch';
+import SearchContainer from './components/search/searchContainer';
 
 function App(props) {
   return (
     <BrowserRouter>
     <div className='app-wrapper'>
-      <Seacrh/>
+      <SearchContainer/>
       <Nav/>
       <Routes>
         <Route path='/profile' element={<Content/>}/>
