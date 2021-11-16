d72a01cd0a2e74cfa1aa1b494e8378784476a685
darkknight141 <sashaannenkov4@gmail.com>
2021-11-16 18:36:07 +0300

    Add 'searchReducer' in 'combineReducers' function

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import profileReducer from './profileReducer';
 import messageReducer from './messageReducer';
+import searchReducer from './searchReducer';
 
 let reducers = combineReducers({
     profilePage:profileReducer,
     messagePage:messageReducer,
+    searchPage:searchReducer,
 
 });
 
